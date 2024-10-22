4b51e88332115e229bbee7003f892035ad9d14e7
misaeluno <misaeluno@gmail.com>
2024-10-22 10:43:05 -0300

    LED8x8 1088AS prueba

--- LED8x8_prueba/LED8x8_prueba.ino
+++ LED8x8_prueba/LED8x8_prueba.ino
@@ -13,10 +13,11 @@ void loop(){
 
   for (int j=2; j<10; j++){
    digitalWrite(j, HIGH);
-    for(int k=10; k<18; k++){
+    for(int k=10; k<18; k++){    
      digitalWrite(k, LOW);
      delay(250);
      digitalWrite(k, HIGH);
  }
  digitalWrite(j, LOW);
+}
 }
