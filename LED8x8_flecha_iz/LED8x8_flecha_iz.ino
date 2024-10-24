842616548f98317e3ca844ce5805deb9bb0bee7a
misaeluno <misaeluno@gmail.com>
2024-10-24 13:15:34 -0300

    mejora de LED8x8

--- LED8x8_flecha_iz/LED8x8_flecha_iz.ino
+++ LED8x8_flecha_iz/LED8x8_flecha_iz.ino
@@ -1,4 +1,4 @@
-int i;
+int cont;
 int j;
 int k;
 
@@ -18,278 +18,315 @@ void loop(){
 //  LOW   enciede   fila
 ///////////////////////////////////////////////////////////////
           //Flecha izquierda  
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+  while(cont<1000){  
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(15, LOW);
-  digitalWrite(4, HIGH);
-  //delay(2000);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(15, LOW);
+    digitalWrite(4, HIGH);
+    //delay(2000);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(13, LOW);
-  digitalWrite(4, HIGH);
-  //delay(2000);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(13, LOW);
+    digitalWrite(4, HIGH);
+    //delay(2000);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(12, LOW);
-  digitalWrite(5, HIGH);
-  //delay(2000);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(5, HIGH);
+    //delay(2000);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(16, LOW);
-  digitalWrite(5, HIGH);
-  //delay(2000);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(16, LOW);
+    digitalWrite(5, HIGH);
+    //delay(2000);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(11, LOW);
-  digitalWrite(6, HIGH);
-  //delay(2000);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(6, HIGH);
+    //delay(2000);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(17, LOW);
-  digitalWrite(6, HIGH);
-  //delay(2000);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(17, LOW);
+    digitalWrite(6, HIGH);
+    //delay(2000);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(15, LOW);
-  digitalWrite(4, HIGH);
-  //delay(2000);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(15, LOW);
+    digitalWrite(4, HIGH);
+    //delay(2000);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  for ( j=3; j<10; j++){
-    digitalWrite(j, HIGH);
-    for( k=11; k<18; k++){
-      if(k==14 & j==3){
-        digitalWrite(k, LOW);
+    for ( j=3; j<10; j++){
+      digitalWrite(j, HIGH);
+      for( k=11; k<18; k++){
+        if(k==14 & j==3){
+          digitalWrite(k, LOW);
+        }
+      }
+      //delay(2000);
+      digitalWrite(j, LOW);
+    }
+    //delay(2000);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
       }
     }
     //delay(2000);
-    digitalWrite(j, LOW);
+    cont++;
   }
-  //delay(2000);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
-    }
-  }
-  //delay(2000);
 ///////////////////////////////////////////////////////////////
-
+cont=0;
 ///////////////////////////////////////////////////////////////
             //Flecha derecha
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
-    }
-  }
-  digitalWrite(15, LOW);
-  digitalWrite(8, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
-    }
-  }
-  digitalWrite(13, LOW);
-  digitalWrite(8, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
-    }
-  }
-  digitalWrite(12, LOW);
-  digitalWrite(7, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
-    }
-  }
-  digitalWrite(16, LOW);
-  digitalWrite(7, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
-    }
-  }
-  digitalWrite(11, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
-    }
-  }
-  digitalWrite(17, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
-    }
-  }
-  for ( j=3; j<10; j++){
-    digitalWrite(j, HIGH);
-    for( k=11; k<18; k++){
-      if(k==14 & j==3){
-        digitalWrite(k, LOW);
+  while(cont<1000){  
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
       }
     }
-    digitalWrite(j, LOW);
-  }
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(15, LOW);
+    digitalWrite(8, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
+    digitalWrite(13, LOW);
+    digitalWrite(8, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+    digitalWrite(12, LOW);
+    digitalWrite(7, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+    digitalWrite(16, LOW);
+    digitalWrite(7, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+    digitalWrite(11, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+    digitalWrite(17, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+    for ( j=3; j<10; j++){
+      digitalWrite(j, HIGH);
+      for( k=11; k<18; k++){
+        if(k==14 & j==3){
+          digitalWrite(k, LOW);
+        }
+      }
+      digitalWrite(j, LOW);
+    }
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+    cont++;
   }
 ///////////////////////////////////////////////////////////////
-
+  cont=0;
 ///////////////////////////////////////////////////////////////
             //Linea continua
-  for ( j=3; j<10; j++){
-    digitalWrite(j, HIGH);
-    for( k=11; k<18; k++){
-      if(k==14 & j==3){
-        digitalWrite(k, LOW);
+  while(cont<10000){
+    for ( j=3; j<10; j++){
+      digitalWrite(j, HIGH);
+      for( k=11; k<18; k++){
+        if(k==14 & j==3){
+          digitalWrite(k, LOW);
+        }
       }
+      digitalWrite(j, LOW);
     }
-    digitalWrite(j, LOW);
+    cont++;
   }
 ///////////////////////////////////////////////////////////////
-
+  cont=0;
 ///////////////////////////////////////////////////////////////
             //exclamacion
-  digitalWrite(17, LOW);
-  digitalWrite(5, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+  while(cont<1000){
+    digitalWrite(17, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(17, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(15, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(17, LOW);
-  digitalWrite(7, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(14, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(16, LOW);
-  digitalWrite(5, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(13, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(16, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(13, LOW);
+    digitalWrite(7, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(16, LOW);
-  digitalWrite(7, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(13, LOW);
+    digitalWrite(5, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(15, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(14, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(7, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(13, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(5, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(12, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(11, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(7, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
-  }
-  digitalWrite(10, LOW);
-  digitalWrite(6, HIGH);
-  for ( j=2; j<10; j++){
-    digitalWrite(j, LOW);
-    for ( k=10; k<18; k++){
-      digitalWrite(k, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(5, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
     }
+    digitalWrite(10, LOW);
+    digitalWrite(6, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+    digitalWrite(10, LOW);
+    digitalWrite(7, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+    digitalWrite(10, LOW);
+    digitalWrite(5, HIGH);
+    for ( j=2; j<10; j++){
+      digitalWrite(j, LOW);
+      for ( k=10; k<18; k++){
+        digitalWrite(k, HIGH);
+      }
+    }
+  cont++;
   }
 ///////////////////////////////////////////////////////////////
+  cont=0;
 }
