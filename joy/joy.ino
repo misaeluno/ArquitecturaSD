0385280466814bffc51a839d9ad978b6ea9c77ec
Celes <celestemarmar2@gmail.com>
2024-10-24 14:22:20 -0300

    Kind of works

--- joy/joy.ino
+++ joy/joy.ino
@@ -4,10 +4,10 @@
 int x = 0;
 int y = 0;
 
-int ama1 = 3;//derecha
-int ama2 = 3;//arriba
-int roj1 = 2;//izquierda
-int roj2 = 2;//abajo
+int ama1 = 12;//derecha
+int ama2 = 10;//abajo
+int roj1 = 9;//izquierda
+int roj2 = 11;//arriba
 float calc;
 
 void setup() {
@@ -22,24 +22,21 @@ void loop() {
  x = analogRead(VRX); 
  y = analogRead(VRY);
 
- calc = ((float) (x - 512)) / 2;
+ calc = ((float) (x - 512)) / 8;
  calc = calc > 0 ? calc : 0;
  analogWrite(ama1, calc);
- calc = ((float) (x - 512)) / 2;
- calc = -calc > 0 ? -calc : 0;
+ 
+calc = ((float) (x - 512)) / 8;
+ calc = -calc > 0 ? 256 - calc : 0;
  analogWrite(roj1, calc);
 
- calc = ((float) (y - 512)) / 2;
+ calc = ((float) (y - 512)) / 8;
  calc = calc > 0 ? calc : 0;
  analogWrite(ama2, calc);
- calc = ((float) (y - 512)) / 2;
- calc = -calc > 0 ? -calc : 0;
+
+ calc = ((float) (y - 512)) / 8;
+ calc = -calc > 0 ? 256 - calc : 0;
  analogWrite(roj2, calc);
 
- // print data to Serial Monitor on Arduino IDE
- Serial.print("x = ");
- Serial.print(x);
- Serial.print(", y = ");
- Serial.println(y);
- delay(200);
+ delay(100);
 }
