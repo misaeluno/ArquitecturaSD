593ae9a144abad247887390d63064268a6a01e8d
misaeluno <misaeluno@gmail.com>
2024-12-09 22:54:48 -0300

    Trabajo final, Se descubre que el cabliado esta mal

--- Proyecto final/Proyecto_Final/Proyecto_Final.ino
+++ Proyecto final/Proyecto_Final/Proyecto_Final.ino
@@ -4,25 +4,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Id Horario;
-typedef struct Node Clases;
+typedef struct Horario Horario;
+typedef struct Node Node;
+typedef struct List List;
 
-struct Id {       //estructura de las IDs
-  int Dia[5];       //01) Lunes        |02) Martes        |03) Miercoles     |04) Jueves       |05) Viernes 
-  int Bloque[11];   //01) 08:00->0930  |02) 09:30->09:40  |03) 09:40->11:10  |04) 11:10->11:20 |05) 11:20->12:50 |06) 12:50->14:00
-};                  //07) 14:00->15:30 |08) 15:30->15:40  |09) 15:40->17.10  |10) 17:10->17:20 |11) 17:20->18:50 |
-
-struct Node {     //Node que contiene los IDs de las tarjetas
-  Id Horario;
-  char Clase[99];
-  Node *prev;
-  Node *next;
+struct Horario {       //estructura de las IDs
+  unsigned long incio;   
+  unsigned long fin;    //01) 08:00->0930  |02) 09:30->09:40  |03) 09:40->11:10  |04) 11:10->11:20 |05) 11:20->12:50 |06) 12:50->14:00
+  char evento;          //07) 14:00->15:30 |08) 15:30->15:40  |09) 15:40->17.10  |10) 17:10->17:20 |11) 17:20->18:50 |
 };
 
-//Inicializa funciones (prototipo)
-//estas funciones se definen al final del codigo
-Node *CreateNode(Id Horario);                       //Se crea la funcion "CreateNode" para poder crear un node
+struct List {
+  int tamano;
+  Node *cabeza;
+  Node *cola
+};
 
+struct Node {     //Node que contiene los IDs de las tarjetas
+  Horaio *horaio;
+  Node *sigiente;
+  Node *anteriaor;
+};
+
+List horarios[5] = { 0 };
+char *nombre_Evento = { "Disponible", "Receso", "ALmuerzo", "Lenguaje1", "Lenguaje2" };
+int Append(List *horario{inicio,fin,nombre_Evento});
 void setup() {
   // put your setup code here, to run once:
 
--- Proyecto final/Prueba_Pantalla_LED1502A/Prueba_Pantalla_LED1502A.ino
+++ Proyecto final/Prueba_Pantalla_LED1502A/Prueba_Pantalla_LED1502A.ino
@@ -1,15 +1,16 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(7, 8, 9, 10, 11 , 12);
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() { 
-
-  lcd.begin(16, 2);
-
-  lcd.setCursor(0,1);
-
-  lcd.witre(»Hola«);
-
+  lcd.begin(16, 2);  // Inicializa el LCD con 16 columnas y 2 filas
+  lcd.setCursor(0, 0);  // Coloca el cursor en la segunda fila (0 es la primera, 1 es la segunda)
+  lcd.write("hola");  // Usa print en lugar de write para mostrar el texto
+lcd.print("Hola Mundo!");
+delay(1000);
+lcd.clear();
+lcd.setCursor(0, 1);  // Cambiar a la segunda fila
+lcd.print("Segunda Fila");
 
 }
 
