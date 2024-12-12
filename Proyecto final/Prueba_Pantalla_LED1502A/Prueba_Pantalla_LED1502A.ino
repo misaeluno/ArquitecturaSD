85f48f5d7ba0c26baabd88d80c0a612dc52380f4
misaeluno <misaeluno@gmail.com>
2024-12-12 09:14:13 -0300

    funcion con hora falta probar

--- Proyecto final/Prueba_Pantalla_LED1502A/Prueba_Pantalla_LED1502A.ino
+++ Proyecto final/Prueba_Pantalla_LED1502A/Prueba_Pantalla_LED1502A.ino
@@ -14,6 +14,8 @@ void setup() {
 }
 
 void Clase(String dato);
+void Bloques(String dato, int hora);
+void Materias(String dato, int hora);
 
 void Clase(String dato){
   lcd.setCursor(0,0);
@@ -21,15 +23,39 @@ void Clase(String dato){
   lcd.setCursor (0,1);
   lcd.print("Juego de rol DYD");
   delay(5000);
-  lcd.noBacklight(); // Apaga la retroiluminación después de mostrar el mensaje
-  delay(2000); // Mantiene la pantalla apagada durante 2 segundos
-  lcd.backlight(); // Vuelve a encender la retroiluminación
+  //lcd.noBacklight(); // Apaga la retroiluminación después de mostrar el mensaje
+  //delay(2000); // Mantiene la pantalla apagada durante 2 segundos
+  //lcd.backlight(); // Vuelve a encender la retroiluminación
 }
+
+void Bloques(String dato,int hora){
+  if(hora==0){
+    lcd.setCursor(0,0);
+    lcd.print(dato);
+    delay(5000);
+  }
+  else if(hora==1){
+    lcd.setCursor(0,1);
+    lcd.print(dato);
+    delay(5000);
+  }
+}
+
+void Materias(String dato, int hora){
+  //intertar con swhis
+} 
 void loop() { 
+  int hora=0;
   String clase="DA";
   //lcd.display();
   //delay(1000);
   //lcd.noDisplay();
   //delay(50);
   Clase(clase);
+  delay(2000);
+  Bloques(clase,hora);
+  delay(2000);
+  hora++;
+  Bloques(clase,hora);
+  delay(2000);
 }
